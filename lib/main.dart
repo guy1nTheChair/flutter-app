3013fc439059cff1275c3c2aea09ce947da523fd
Santosh <ssalimath7@gmail.com>
2020-07-17 10:25:32 +0530

    Added Restart Quiz functionality and scores

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import './quiz.dart';
-import './answer.dart';
 import 'result.dart';
 
 void main() => runApp(MyApp());
@@ -14,26 +13,56 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   var _questionIndex = 0;
+  var _totalScore = 0;
+
   final _questions = [
     {
       "questionText": "What's your favourite color?",
-      "answers": ["Blue", "White", "Black", "Red"]
+      "answers": [
+        {"text": "Blue", "score": 6},
+        {"text": "White", "score": 0},
+        {"text": "Black", "score": 10},
+        {"text": "Red", "score": 8},
+      ]
     },
     {
       "questionText": "What's your favourite animal?",
-      "answers": ["Cat", "Dog", "Rat", "Monkey"]
+      "answers": [
+        {"text": "Cat", "score": 2},
+        {"text": "Dog", "score": 2},
+        {"text": "Rat", "score": 4},
+        {"text": "Monkey", "score": 3},
+      ]
     },
     {
       "questionText": "What's your favourite ice cream?",
-      "answers": ["Pista", "Butterscotch", "Chocolate", "Vanilla"]
+      "answers": [
+        {"text": "Pista", "score": 6},
+        {"text": "Butterscotch", "score": 5},
+        {"text": "Chocolate", "score": 3},
+        {"text": "Vanilla", "score": 1}
+      ]
     },
     {
       "questionText": "What's your favourite game?",
-      "answers": ["CSGO", "Apex", "Valorant", "COD"]
+      "answers": [
+        {"text": "CSGO", "score": 6},
+        {"text": "Apex", "score": 4},
+        {"text": "Mario", "score": 3},
+        {"text": "GTA V", "score": 1}
+      ]
     }
   ];
 
-  void _answerQuestion() {
+  void _resetStage() {
+    setState(() {
+      _questionIndex = 0;
+      _totalScore = 0;
+    });
+  }
+
+  void _answerQuestion(int score) {
+    _totalScore += score;
     setState(() {
       _questionIndex += 1;
     });
@@ -48,6 +77,6 @@ class _MyAppState extends State<MyApp> {
             ),
             body: _questionIndex < _questions.length
                 ? Quiz(_answerQuestion, _questions, _questionIndex)
-                : Result()));
+                : Result(_totalScore, _resetStage)));
   }
 }
--- lib/result.dart
+++ lib/result.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
 
 class Result extends StatelessWidget {
+  final int totalScore;
+  final Function resetHandler;
+  //final String resultText = "You Did it!! ";
+  Result(this.totalScore, this.resetHandler);
+
+  String get resultText {
+    return ("You Did it!! " + totalScore.toString());
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(child: Center(child: Text("Quiz Complete")));
+    return Container(
+        margin: EdgeInsets.all(25),
+        child: Center(
+            child: Column(
+          children: <Widget>[
+            Text(
+              resultText,
+              style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            RaisedButton(
+              child: Text('Retake Quiz',
+                  style:
+                      TextStyle(fontSize: 20, fontWeight: FontWeight.normal)),
+              onPressed: resetHandler,
+            )
+          ],
+        )));
   }
 }
